8315f961c7f822cfbef0aed423493e48da01aa7c
Nafiul Hasan <89503458+NafiHasan@users.noreply.github.com>
2023-02-11 12:32:45 +0600

    Update Sieve.cpp

--- Templates/Number Theory/Sieve.cpp
+++ Templates/Number Theory/Sieve.cpp
@@ -8,3 +8,17 @@ void sieve(){
 		for (int j = i*i ; j <= N; j+=i)prime[j] = 0;
 	}
 }
+
+// sieve with bitset
+const int N = 10000010;
+bitset < N + 5 > bs;
+vector<int> primes;
+void sieve() {
+	bs.set();
+	bs[0] = bs[1] = 0;
+	for (int i = 2; i * i <= N; i++) {
+		if (!bs[i])continue;
+		for (int j = i * i ; j <= N; j += i)bs[j] = 0;
+	}
+	for (int i = 2; i <= N; i++)if (bs[i])primes.push_back(i);
+}
