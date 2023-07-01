aaf84c76fe40b4828c1a4a8ddb6fca1a0150286d
N-Crypt3d <89503458+N-Crypt3d@users.noreply.github.com>
2023-07-01 15:33:42 +0600

    lca update

---
+++ Templates/Data Structure/LCA (Lowest Common Ancestor)/LCA.cpp
@@ -0,0 +1,48 @@
+//template
+const int N = 200005;
+int timer = 0;
+vector<int> edge[N], tin(N), tout(N);
+vector<vector<int>> par(N, vector<int> (25, -1));
+vector<bool> vis(N, 0);
+int n;
+
+//Run dfs from (root - 1), e.g. dfs(0)
+void dfs(int u) {
+	vis[u] = 1;
+	tin[u] = timer++;
+	for (int v : edge[u]) {
+		if (!vis[v]) {
+			dfs(v);
+			par[v][0] = u;
+		}
+	}
+	tout[u] = timer++;
+}
+
+int isAncestor(int u, int v) {
+	if (tin[u] <= tin[v] && tout[u] >= tout[v])return u;
+	if (tin[v] <= tin[u] && tout[v] >= tout[u])return v;
+	return -1;
+}
+
+void process() {
+	int Log = ceil(log2(n));
+	for (int i = 1; i <= Log; i++) {
+		for (int j = 0; j < n; j++) {
+			if (par[j][i - 1] != -1)par[j][i] = par[par[j][i - 1]][i - 1];
+		}
+	}
+}
+
+int FindLCA(int u, int v) {
+	int cur = u;
+	int Log = ceil(log2(n));
+	cur = isAncestor(u, v);
+	for (int i = Log; i >= 0; i--) {
+		if (par[v][i] != -1 && isAncestor(u, par[v][i]) == -1) {
+			v = par[v][i];
+		}
+	}
+	if (cur == -1)cur = par[v][0];
+	return cur;
+}
